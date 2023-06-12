9848c882d5784d7f768e735bc94b4e4d928c86f1
choi <a@a.com>
2023-06-12 14:30:32 +0900

    day01

---
+++ src/day01/TestClass01.java
@@ -0,0 +1,17 @@
+package day01;
+
+public class TestClass01 {
+	public static void main(String[] args) 
+	{	
+		
+		// 괄호 안의 내용을 출력해주는 기능
+		// ; : 쎄미콜론이며 문장의 마지막에는 꼭 있어야 한다
+		System.out.println(100);		// println : 줄바꿈 기능 O
+		System.out.println("안녕하세요");
+		
+		System.out.print("1.안녕");		// print : 줄바꿈 기능 X
+		System.out.print("2.안녕");
+		System.out.print("3.안녕");
+	}
+
+}
